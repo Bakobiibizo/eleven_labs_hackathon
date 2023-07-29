020a38b3f75e6926e6f5be181f8b59864a409d6b
Richard Bakobiibizo <110358293+Bakobiibizo@users.noreply.github.com>
2023-07-29 02:52:55 -0700

    aider: Modified the `/text` WebSocket route to generate audio from the received text and send it as base64 to the UI, and modified the `/updates` WebSocket route to send the base64 audio to the UI.
    # Aider chat conversation:
    
    USER: this is how im recieving and creating the audio
    
    ASSISTANT: Great, now we need to integrate this with the WebSocket routes we created earlier.
    
    We will modify the `/text` WebSocket route to call the `tts` method of the `TextToSpeach` class when it receives text data. This will generate the audio from the received text.
    
    We will also modify the `/updates` WebSocket route to send the generated audio file to the UI. For this, we will need to convert the audio file to a format that can be sent over WebSocket, such as base64.
    
    Here are the changes to `app.py`:
    
    ```python
    app.py
    <<<<<<< ORIGINAL
    @app.websocket("/text")
    async def websocket_endpoint(websocket: WebSocket):
        await websocket.accept()
        while True:
            data = await websocket.receive_text()
            # Handle the received text data here
    
    @app.websocket("/updates")
    async def websocket_update_endpoint(websocket: WebSocket):
        await websocket.accept()
        while True:
            # Send updates to the UI here
            # You will need to implement the logic for sending MP3 and JPG files
    =======
    from voice.eleven_labs import TextToSpeach
    import base64
    
    tts = TextToSpeach()
    
    @app.websocket("/text")
    async def websocket_endpoint(websocket: WebSocket):
        await websocket.accept()
        while True:
            data = await websocket.receive_text()
            # Generate audio from the received text
            audio_file = tts.tts(data, voice_id=None)
            # Convert the audio file to base64
            with open(audio_file, "rb") as f:
                audio_base64 = base64.b64encode(f.read())
            # Send the base64 audio to the UI
            await websocket.send_text(audio_base64)
    
    @app.websocket("/updates")
    async def websocket_update_endpoint(websocket: WebSocket):
        await websocket.accept()
        while True:
            # Send updates to the UI here
            # You will need to implement the logic for sending MP3 and JPG files
            # For now, we will just send the base64 audio
            await websocket.send_text(audio_base64)
    >>>>>>> UPDATED
    ```
    
    Please note that this is a basic implementation and may not cover all your requirements. You may need to modify it to fit your specific needs. For example, you may want to handle errors, add logging, etc. Also, the `tts` method currently plays the audio immediately after generating it. You may want to modify this behavior depending on your requirements.

--- app.py
+++ app.py
@@ -9,12 +9,23 @@ app.add_middleware(
     CORSMiddleware,
 )
 
+from voice.eleven_labs import TextToSpeach
+import base64
+
+tts = TextToSpeach()
+
 @app.websocket("/text")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        # Handle the received text data here
+        # Generate audio from the received text
+        audio_file = tts.tts(data, voice_id=None)
+        # Convert the audio file to base64
+        with open(audio_file, "rb") as f:
+            audio_base64 = base64.b64encode(f.read())
+        # Send the base64 audio to the UI
+        await websocket.send_text(audio_base64)
 
 @app.websocket("/updates")
 async def websocket_update_endpoint(websocket: WebSocket):
@@ -22,3 +33,5 @@ async def websocket_update_endpoint(websocket: WebSocket):
     while True:
         # Send updates to the UI here
         # You will need to implement the logic for sending MP3 and JPG files
+        # For now, we will just send the base64 audio
+        await websocket.send_text(audio_base64)
